ba5c30233aefa14dac54410ab47df1ae8339fd54
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-01-20 01:49:40 -0500

    beautify with semantic ui

--- src/config.py
+++ src/config.py
@@ -10,8 +10,8 @@ class Config(object):
 
     WEBPACK_MANIFEST_PATH = '/build/manifest.json'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') 
-#        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
+      'postgresql://postgres:P@ssw0rd!@172.17.0.2/simpleblog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # ADMINS = ['michael.sergio.barnes@gmail.com']
