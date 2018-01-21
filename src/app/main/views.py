6e657b98cdc9858014f985a4430b7386458f62e3
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-01-21 02:10:26 -0500

    fix pagination in ui

--- src/app/main/views.py
+++ src/app/main/views.py
@@ -107,7 +107,7 @@ def follow(username):
         return redirect(url_for('main.user', username=username))
     current_user.follow(user)
     db.session.commit()
-    flash('You are following %(username)s!', username=username)
+    # flash('You are following %(username)s!', username=username)
     return redirect(url_for('main.user', username=username))
 
 
@@ -116,12 +116,12 @@ def follow(username):
 def unfollow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User %(username)s not found.', username=username)
+        # flash('User %(username)s not found.', username=username)
         return redirect(url_for('main.index'))
     if user == current_user:
         flash('You cannot unfollow yourself!')
-        return redirect(url_for('main.user', username=username))
+        # return redirect(url_for('main.user', username=username))
     current_user.unfollow(user)
     db.session.commit()
-    flash('You are not following %(username)s.', username=username)
+    # flash('You are not following %(username)s.', username=username)
     return redirect(url_for('main.user', username=username))
