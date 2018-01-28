226fafbd9eee13b3ecce9079a05cb611c5224a9a
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-01-27 20:42:57 -0500

    Add ability to read private messages

--- src/app/main/views.py
+++ src/app/main/views.py
@@ -153,6 +153,7 @@ def search():
     return render_template('search.html', title='Search', posts=posts,
                            next_url=next_url, prev_url=prev_url)
 
+
 @main.route('/send_message/<recipient>', methods=['GET', 'POST'])
 @login_required
 def send_message(recipient):
@@ -170,3 +171,19 @@ def send_message(recipient):
                            form=form, recipient=recipient)
 
 
+@main.route('/messages')
+@login_required
+def messages():
+    current_user.last_message_read_time = datetime.utcnow()
+    current_user.add_notification('unread_message_count', 0)
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    messages = current_user.messages_received.order_by(
+        Message.timestamp.desc()).paginate(
+            page, current_app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main.messages', page=messages.next_num) \
+        if messages.has_next else None
+    prev_url = url_for('main.messages', page=messages.prev_num) \
+        if messages.has_prev else None
+    return render_template('messages.html', messages=messages.items,
+                           next_url=next_url, prev_url=prev_url)
