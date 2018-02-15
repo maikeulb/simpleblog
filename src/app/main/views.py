3231f624c242347ad403b17b8cd09a195de6153b
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-15 17:15:48 -0500

    improve error pages

--- src/app/main/views.py
+++ src/app/main/views.py
@@ -215,3 +215,9 @@ def notifications():
         'data': n.get_data(),
         'timestamp': n.timestamp
     } for n in notifications])
+
+@main.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('main/user_popup.html', user=user)
